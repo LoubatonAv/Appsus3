9870d3768933288e67e85960f159592a42eea402
TsurDrori <tsurdrori@gmail.com>
2022-01-02 00:28:09 +0200

    links changes

--- js/apps/keep/pages/note-app.jsx
+++ js/apps/keep/pages/note-app.jsx
@@ -1,10 +1,9 @@
 import { NoteService } from '../services/Note.service.js'
 import { NoteList } from '../cmps/note-list.jsx'
-import { NoteDetails } from './note-details.jsx'
+import { NoteDetails } from '../note-details.jsx'
 import { AddNote } from '../cmps/add-note.jsx'
-import { Loader } from '../../../cmps/Loader.jsx'
 import { eventBusService } from '../../../services/event-bus.service.js'
-
+import { Loader } from '../cmps/Loader.jsx';
 
 // const { Link } = ReactRouterDOM
 
